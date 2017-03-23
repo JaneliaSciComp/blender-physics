6c51e9456e99385b678458e69c78b84267b4ad73
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-03-23 16:02:33 -0400

    Set camera to top-down orthographic view. Place one smooth-shaded sphere.

--- test.py
+++ test.py
@@ -1,8 +1,29 @@
 import bpy
 
-print ("Hello")
+# Delete the default starting geometry (that cube...)
+# select objects by type
+for o in bpy.data.objects:
+    if o.type == 'MESH':
+        o.select = True
+    else:
+        o.select = False
+# delete the selected items
+bpy.ops.object.delete()
 
-folder = 'C:/Users/brunsc/projects/blender_physics'
-bpy.data.scenes['Scene'].render.filepath = folder + '/test1.png'
+# Insert scene items
+bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=4, size=1)
+bpy.ops.object.shade_smooth()
+
+# Set camera to top-down view
+cam = bpy.context.scene.camera
+cam.rotation_euler = [0, 0, 0]
+cam.location = [0, 0, 10]
+# Use orthographic projection for maximum simplicity
+cam.data.type = 'ORTHO'
+cam.data.ortho_scale = 20
+
+# Save screenshot to disk
+img_folder = 'C:/Users/brunsc/projects/blender_physics'
+scene = bpy.data.scenes['Scene']
+scene.render.filepath = img_folder + '/test1.png'
 bpy.ops.render.render( write_still=True ) 
-
