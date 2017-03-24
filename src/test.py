a660ccfa31dc8ef4559443da3f0a3e2eec56e28b
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-03-24 09:24:31 -0400

    Compartmentalize some functions.

--- src/test.py
+++ src/test.py
@@ -1,3 +1,9 @@
+"""
+Blender Python script to generate animation sequence of physically interacting objects.
+"""
+
+import os
+
 import bpy
 
 def delete_all_meshes():
@@ -15,75 +21,79 @@ def delete_all_meshes():
     # delete the selected items
     bpy.ops.object.delete()
 
-delete_all_meshes()
+def set_camera_to_top_down_view(cm_per_screen=10):
+    cam = bpy.context.scene.camera
+    cam.rotation_euler = [0, 0, 0]
+    cam.location = [0, 0, 10]
+    # Use orthographic projection for maximum simplicity
+    cam.data.type = 'ORTHO'
+    cam.data.ortho_scale = cm_per_screen # centimeters per screen
 
-# Insert scene items
-# Create material for uniform flat coloring
-mat = bpy.data.materials.new('SurfaceMat')
-mat.use_shadeless = True # cartoonish solid color
-# Insert spheres at various locations
-objects = []
-for loc in [ [0,0,0], [1,3,0], ]:
-    # Create sphere object
-    bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=4, size=1, location=loc)
-    # Use smooth shading (interpolate surface normals)
-    bpy.ops.object.shade_smooth()
-    # Apply our material
-    ob = bpy.context.active_object
-    if ob.data.materials:
-        ob.data.materials[0] = mat
-    else:
-        ob.data.materials.append(mat)
-    # Activate physics on this object
-    bpy.ops.rigidbody.object_add()
-    # Use spherical collision boundary
-    ob.rigid_body.collision_shape = 'SPHERE'
-    # Save this object in a list, for later access
-    objects.append(ob)
+def main():
+    delete_all_meshes()
+    set_camera_to_top_down_view()
 
-# Set camera to top-down view
-cam = bpy.context.scene.camera
-cam.rotation_euler = [0, 0, 0]
-cam.location = [0, 0, 10]
-# Use orthographic projection for maximum simplicity
-cam.data.type = 'ORTHO'
-cam.data.ortho_scale = 10 # meters per screen
+    # Insert scene items
+    # Create material for uniform flat coloring
+    mat = bpy.data.materials.new('SurfaceMat')
+    mat.use_shadeless = True # cartoonish solid color
+    # Insert spheres at various locations
+    objects = []
+    for loc in [ [0,0,0], [1,3,0], ]:
+        # Create sphere object
+        bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=4, size=1, location=loc)
+        # Use smooth shading (interpolate surface normals)
+        bpy.ops.object.shade_smooth()
+        # Apply our material
+        ob = bpy.context.active_object
+        if ob.data.materials:
+            ob.data.materials[0] = mat
+        else:
+            ob.data.materials.append(mat)
+        # Activate physics on this object
+        bpy.ops.rigidbody.object_add()
+        # Use spherical collision boundary
+        ob.rigid_body.collision_shape = 'SPHERE'
+        # Save this object in a list, for later access
+        objects.append(ob)
 
-rend = bpy.context.scene.render
-rend.engine = 'BLENDER_RENDER'
-rend.resolution_x = 1024
-rend.resolution_y = 1024
-scene = bpy.data.scenes['Scene']
-scene.gravity = [0,0,0] # cm/sec**2
+    rend = bpy.context.scene.render
+    rend.engine = 'BLENDER_RENDER'
+    rend.resolution_x = 1024
+    rend.resolution_y = 1024
+    scene = bpy.data.scenes['Scene']
+    scene.gravity = [0,0,0] # cm/sec**2
 
-# Manipulate keyframes to set initial velocity
-scene.frame_start = 1
-scene.frame_end = 101
-fps = 30.0
-v_i = [0.0, -1.0, 0.0]
-dLoc = [x / fps for x in v_i]
-# Set initial velocity by setting location in frame start-1
-ob = objects[1]
-# Select only the one object
-for obj in bpy.data.objects:
-    obj.select = False
-ob.select = True
-# Location before first frame
-loc0 = [ob.location[x] - dLoc[x] for x in range(3)]
-loc1 = [x for x in ob.location]
-scene.frame_set(frame=scene.frame_start-1) # zeroth frame
-ob.rigid_body.kinematic = True
-ob.location = loc0
+    # Manipulate keyframes to set initial velocity
+    scene.frame_start = 1
+    scene.frame_end = 101
+    fps = 30.0
+    v_i = [0.0, -1.0, 0.0]
+    dLoc = [x / fps for x in v_i]
+    # Set initial velocity by setting location in frame start-1
+    ob = objects[1]
+    # Select only the one object
+    for obj in bpy.data.objects:
+        obj.select = False
+    ob.select = True
+    # Location before first frame
+    loc0 = [ob.location[x] - dLoc[x] for x in range(3)]
+    loc1 = [x for x in ob.location]
+    scene.frame_set(frame=scene.frame_start-1) # zeroth frame
+    ob.rigid_body.kinematic = True
+    ob.location = loc0
 
-import os
-src_folder = os.path.dirname(os.path.realpath(__file__))
-img_folder = os.path.join(src_folder, 'frames')
-# Save screenshot to disk
-scene.render.filepath = img_folder + '/test1.png'
-bpy.ops.render.render( write_still=True ) 
+    src_folder = os.path.dirname(os.path.realpath(__file__))
+    img_folder = os.path.join(src_folder, 'frames')
+    # Save screenshot to disk
+    scene.render.filepath = img_folder + '/test1.png'
+    bpy.ops.render.render( write_still=True ) 
 
-scene.frame_set(frame=scene.frame_start) # first frame
-ob.location = loc1
-# Save screenshot to disk
-scene.render.filepath = img_folder + '/test2.png'
-bpy.ops.render.render( write_still=True ) 
+    scene.frame_set(frame=scene.frame_start) # first frame
+    ob.location = loc1
+    # Save screenshot to disk
+    scene.render.filepath = img_folder + '/test2.png'
+    bpy.ops.render.render( write_still=True ) 
+
+if __name__ == '__main__':
+    main()
